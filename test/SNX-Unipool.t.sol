15d972bd5762be979d09dfcb00a9bb0bb40d7190
aewc <flyq951@gmail.com>
2023-02-27 10:33:58 +0800

    add test

--- test/SNX-Unipool.t.sol
+++ test/SNX-Unipool.t.sol
@@ -15,22 +15,31 @@ contract UnipoolTest is Test {
     MyToken stakedToken;
 
     function setUp() external {
-        rewardToken = new MyToken("Reward Test Token", "RTT", 0);
+        rewardToken = new MyToken("Reward Test Token", "RTT", 1000_000);
         stakedToken = new MyToken("Staked Test Token", "STT", 1000_000);
         unipool = new Unipool(address(rewardToken), address(stakedToken));
+        rewardToken.transfer(address(unipool), 1000_000*10**stakedToken.decimals());
     }
 
     // VM Cheatcodes can be found in ./lib/forge-std/src/Vm.sol
     // Or at https://github.com/foundry-rs/forge-std
     function testStake() external {
+        emit log_uint(unipool.balanceOf(address(this)));
+        emit log_uint(rewardToken.balanceOf(address(this)));
+        emit log_uint(stakedToken.balanceOf(address(this)));
+
         stakedToken.approve(address(unipool), stakedToken.balanceOf(address(this)));
 
         unipool.stake(1000*10**stakedToken.decimals());
 
-        // // We can read slots directly
-        // uint256 slot = stdstore.target(address(greeter)).sig(greeter.owner.selector).find();
-        // assertEq(slot, 1);
-        // bytes32 owner = vm.load(address(greeter), bytes32(slot));
-        // assertEq(address(this), address(uint160(uint256(owner))));
+        emit log_uint(stakedToken.balanceOf(address(this)));
+
+        skip(7 days);
+        emit log_uint(unipool.earned(address(this)));
+        unipool.withdraw(1000*10**stakedToken.decimals());
+
+        emit log_uint(unipool.balanceOf(address(this)));
+        emit log_uint(rewardToken.balanceOf(address(this)));
+        emit log_uint(stakedToken.balanceOf(address(this)));
     }
 }
